6434d5887be41096dd44005d26a74118b2a92315
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-21 15:12:41 -0300

    feat: add side-bar navigation with icons and labels

---
+++ resources/views/layouts/side_bar.blade.php
@@ -0,0 +1,33 @@
+<aside class="side-bar">
+    <nav>
+        <a href="#" class="nav-item">
+            <i class="icon_account_balance_wallet"></i>
+            {{ __('Sales') }}
+        </a>
+
+        <a href="#" class="nav-item">
+            <i class="icon_garage_home"></i>
+            {{ __('Warehouse') }}
+        </a>
+
+        <a href="#" class="nav-item">
+            <i class="icon_build"></i>
+            {{ __('Tools Control') }}
+        </a>
+
+        <a href="#" class="nav-item">
+            <i class="icon_shopping_bag"></i>
+            {{ __('Purchase') }}
+        </a>
+
+        <a href="#" class="nav-item">
+            <i class="icon_factory"></i>
+            {{ __('Manufacturing') }}
+        </a>
+
+        <a href="#" class="nav-item">
+            <i class="icon_badge"></i>
+            {{ __('Human Resources') }}
+        </a>
+    </nav>
+</aside>
