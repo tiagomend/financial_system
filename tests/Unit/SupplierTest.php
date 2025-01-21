d316a27241bb5e7166aed332ac1eb7483d110818
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-21 17:35:42 -0300

    test: add test so that the trade_name field is nullable

--- tests/Unit/SupplierTest.php
+++ tests/Unit/SupplierTest.php
@@ -11,12 +11,12 @@ class SupplierTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_create_supplier(): void
+    public function test_create_supplier_legal(): void
     {
         $data = [
             'name' => 'Company SA',
             'trade_name' => 'Company',
-            'person_type' => PersonType::NATURAL
+            'person_type' => PersonType::LEGAL
         ];
 
         $supplier = Supplier::create($data);
@@ -25,6 +25,22 @@ class SupplierTest extends TestCase
         $this->assertInstanceOf(Supplier::class, $supplier);
         $this->assertEquals('Company SA', $supplier->name);
         $this->assertEquals('Company', $supplier->trade_name);
+        $this->assertEquals(PersonType::LEGAL, $supplier->person_type);
+    }
+
+    public function test_create_supplier_natural(): void
+    {
+        $data = [
+            'name' => 'Company SA',
+            'person_type' => PersonType::NATURAL
+        ];
+
+        $supplier = Supplier::create($data);
+
+        $this->assertDatabaseHas('suppliers', $data);
+        $this->assertInstanceOf(Supplier::class, $supplier);
+        $this->assertEquals('Company SA', $supplier->name);
+        $this->assertNull($supplier->trade_name);
         $this->assertEquals(PersonType::NATURAL, $supplier->person_type);
     }
 }
