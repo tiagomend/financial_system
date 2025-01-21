90f2c8a88ae1a7f909022fa69dad0b77d723ff6c
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-21 16:18:01 -0300

    refactor: changes the trade_name field to be nullable

--- database/migrations/2025_01_21_135337_create_suppliers_table.php
+++ database/migrations/2025_01_21_135337_create_suppliers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('trade_name', 50);
+            $table->string('trade_name', 50)->nullable();
             $table->enum('person_type', ['legal', 'natural']);
             $table->timestamps();
         });
