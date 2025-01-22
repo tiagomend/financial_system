9ca97f50aa64093d6085839b59adeccf7c7fe67c
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-22 11:52:42 -0300

    test: add test for creating supplier with associated document

--- tests/Unit/SupplierTest.php
+++ tests/Unit/SupplierTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use App\Models\Supplier;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Enums\PersonType;
+use App\Models\Document;
+use App\Models\SupplierDocument;
 use Tests\TestCase as TestCase;
 
 class SupplierTest extends TestCase
@@ -42,5 +44,27 @@ class SupplierTest extends TestCase
         $this->assertEquals('Company SA', $supplier->name);
         $this->assertNull($supplier->trade_name);
         $this->assertEquals(PersonType::NATURAL, $supplier->person_type);
+    }
+
+    public function test_create_supplier_with_document(): void
+    {
+        $data = [
+            'name' => 'Company SA',
+            'trade_name' => 'Company',
+            'person_type' => PersonType::LEGAL
+        ];
+
+        $supplier = Supplier::create($data);
+        $document = Document::create([
+            'type' => 'cnpj',
+            'value' => '88823401000192'
+        ]);
+        SupplierDocument::create([
+            'supplier_id' => $supplier->id,
+            'document_id' => $document->id,
+        ]);
+
+        $this->assertCount(1, $supplier->documents);
+        $this->assertTrue($supplier->documents->contains($document));
     }
 }
