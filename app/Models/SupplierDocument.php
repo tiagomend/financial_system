461d6cd2d3d37e810ffa26d0183d364b3effc393
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-22 11:49:35 -0300

    feat: create SupplierDocument model with relationships to Supplier and Document

---
+++ app/Models/SupplierDocument.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierDocument extends Model
+{
+    protected $fillable = [
+        'supplier_id',
+        'document_id',
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
+}
