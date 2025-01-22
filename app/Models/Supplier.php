b26660ca33ad283ef23824e1c4f6ab4f69c2e82f
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-22 11:51:23 -0300

    refactor: add UUID support to Supplier model, include document relationship and new attribute

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,21 +4,39 @@ namespace App\Models;
 
 use App\Enums\PersonType;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     protected $fillable = [
         'name',
         'trade_name',
         'person_type',
+        'document'
     ];
 
     protected function personType(): Attribute
     {
         return Attribute::make(
             get: fn($value) => $value instanceof PersonType ? $value : PersonType::from($value),
+        );
+    }
+
+    public function documents()
+    {
+        return $this->hasManyThrough(
+            Document::class,
+            SupplierDocument::class,
+            'supplier_id',
+            'id',
+            'id',
+            'document_id'
         );
     }
 }
--- database/migrations/2025_01_21_135337_create_suppliers_table.php
+++ database/migrations/2025_01_21_135337_create_suppliers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('suppliers', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name', 50);
             $table->string('trade_name', 50)->nullable();
             $table->enum('person_type', ['legal', 'natural']);
