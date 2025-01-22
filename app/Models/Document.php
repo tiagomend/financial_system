af62804c454829b05a9c58edab6330943a7b742a
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2025-01-22 11:48:52 -0300

    feat: add Document model with UUID support and relationship to Supplier

---
+++ app/Models/Document.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['type', 'value'];
+
+    public function suppliers()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_documents', 'document_id', 'supplier_id');
+    }
+}
